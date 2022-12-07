44b8be6dcacf1c1968b10f3669c9faf9fe879cae
Oscar Mora <oscgmora@gmail.com>
2022-12-06 22:56:36 -0500

    Fix Container Size & Fix User Mouse Inputs
    This commit fixes the container div so that it does not expandi and only the divs in the child expand or shrink in size to fit the container div. Fixes to the mouse inputs were updated to allow the user to click and drag the mouse to draw.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-for (let i = 0; i < 256; i++) {
-    let gridElement = document.createElement('div');
-    gridElement.className = "gridElement";
-    gridElement.addEventListener('mouseover', changeColor);
-    gridElement.style.background = "black";
-    gridElement.style.height = "20px";
-    gridElement.style.width = "20px";
-    document.getElementById('grid').appendChild(gridElement);
-};
+const defaultSize = 16;
+
+let size = defaultSize;
+let repeater;
+let mouseDown;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function changeColor(e) {
-    e.target.style.backgroundColor = '#fefefe';
+    if (e.type === 'mouseover' && !mouseDown) {
+        return;
+    } else {e.target.style.backgroundColor = '#fefefe'};
 };
 
 let slider = document.getElementById('myRange');
@@ -17,5 +17,39 @@ let output = document.getElementById('demo');
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
-    output.innerHTML = this.value;
+    size = this.value;
+};
+
+slider.onchange = function() {
+    output.innerHTML = `${size} X ${size}`;
+    clearGrid();
+    setupGrid();
 }
+
+function setupGrid() {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    output.innerHTML = `${size} X ${size}`;
+    for (let i = 0; i < size * size; i++) {
+        let gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.style.background = "black";
+        document.getElementById('grid').appendChild(gridElement);
+        gridElement.addEventListener('mouseover', changeColor);
+        gridElement.addEventListener('mousedown', changeColor);
+    };
+};
+
+window.onload = () => {
+    setupGrid(defaultSize);
+    output.innerHTML = `${size} X ${size}`;
+};
+
+function clearGrid () {
+    grid.innerHTML = '';
+};
+
+function reloadGrid () {
+    clearGrid();
+    setupGrid();
+};
